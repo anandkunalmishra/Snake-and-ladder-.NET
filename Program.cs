62b77ee01e0e4993b0d1e18f20791d21d41b02ce
Anand Kunal Mishra <73222935+anandkunalmishra@users.noreply.github.com>
2024-01-22 09:29:21 +0530

    The Snake and Ladder game begins

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Snake_And_Ladder
+{
+    class Gameplay
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("The Game Writing Process begins");
+
+            //The below line protects the program from stopping abruptly
+            Console.ReadLine();
+        }
+    }
+}
